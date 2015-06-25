50bf1efe93e6a60ea1c854ea427b4b29975bf9c9
Tim Lebo <timrdf@gmail.com>
2015-06-25 23:19:58 +0000

    retrieve and prepare cv-foundation.org pdf

---
+++ data/source/cv-foundation-org/openaccess/version/wget/prepare.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#3> @prefix doap:    <http://usefulinc.com/ns/doap#> .
+#3> @prefix dcterms: <http://purl.org/dc/terms/> .
+#3> @prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#> .
+#3> 
+#3> <> a conversion:RetrievalTrigger, doap:Project; # Could also be conversion:Idempotent;
+#3>    dcterms:description 
+#3>      "Script to retrieve and convert a new version of the dataset.";
+#3>    rdfs:seeAlso 
+#3>      <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Automated-creation-of-a-new-Versioned-Dataset>,
+#3>      <https://github.com/timrdf/csv2rdf4lod-automation/wiki/tic-turtle-in-comments>;
+#3> .
+
+mkdir -p automatic
+for pdf in `find 'source/www.cv-foundation.org/openaccess' -mindepth 3 -maxdepth 3 -name *.pdf`; do
+
+   echo $pdf
+   dir=`dirname $pdf | sed 's/^source/automatic/'`
+   base=`basename $pdf`
+   base=${base/%pdf/txt}
+   txt="$dir/$base"
+
+   if [[ ! -e $txt ]]; then
+      echo "    $dir"
+      mkdir -p $dir 
+      echo "    $txt" 
+      pdftotext $pdf $txt
+      #justify.sh $pdf $txt 'http://dbpedia.org/resource/Pdftotext'
+      echo
+   fi
+done
