009b0de58d7021728088b64ccc1100c941038904
Tim L <timrdf@gmail.com>
2015-07-10 18:08:39 -0400

    moving saxon.sh call to use its new shiny --memory and -D options

--- data/source/dblp.uni-trier.de/dblp/version/latest/convert.sh
+++ data/source/dblp.uni-trier.de/dblp/version/latest/convert.sh
@@ -13,5 +13,6 @@ fi
 
 echo automatic/dblp.xml.ttl
 if [[ ! -e automatic/dblp.xml.ttl ]]; then
-   saxon.sh ../../src/grddl.xsl xml ttl automatic/dblp.xml > automatic/dblp.xml.ttl
+   saxon.sh --memory 16 -D '-DentityExpansionLimit=1000000' --using ../../src/grddl.xsl xml ttl automatic/dblp.xml > automatic/dblp.xml.ttl
+   grep 'dblp:primaryElectronicEdition' automatic/dblp.xml.ttl | awk '{print $2}' | sort -u > automatic/dblp.xml.ttl.ee.ttl
 fi
